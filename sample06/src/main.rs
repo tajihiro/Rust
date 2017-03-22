a9962f97d49d054af5b5797d029a251cfe6dda03
H.Tajima <tajihiro2@gmail.com>
2017-03-23 00:17:49 +0900

    Modified Max and Min Function.

--- sample06/src/main.rs
+++ sample06/src/main.rs
@@ -1,14 +1,14 @@
 fn main() {
     let my_array = vec![61,14,71,23,42,8,13,66];
-    let max = max(my_array);
-    let min = min(my_array);
+    let max = max(&my_array);
+    let min = min(&my_array);
     println!("Max value is {}.", max);
-    //println!("Min value is {}.", min);
+    println!("Min value is {}.", min);
 }
 
-fn max(array: Vec<i32>) -> i32{
+fn max(array: &[i32]) -> i32{
     let mut max = array[0];
-    for val in array{
+    for &val in array{
         if max < val {
             max = val;
         }
@@ -16,13 +16,12 @@ fn max(array: Vec<i32>) -> i32{
     max
 }
 
-fn min(array: Vec<i32>) -> i32{
+fn min(array: &[i32]) -> i32{
     let mut min = array[0];
-    for val in array{
-        if min < val {
+    for &val in array{
+        if min > val {
             min = val;
         }
     }
     min
 }
-
